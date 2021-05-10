bfe333c9fd59da56ac52be82465dcd8782a94df0
Ranjana1208 <patelranjanalakshmi@gmail.com>
2021-05-10 16:30:07 +0530

    Login Page Completed

---
+++ Example1.java
@@ -0,0 +1,12 @@
+package day1Example;
+
+public class Example1 {
+	public static void main(String[] args) {
+		System.out.println("Hi");
+		System.out.print("Welcome");
+		System.out.print("Lakshmi");
+		System.out.println("Ranjana");
+		System.out.print("World");
+	}
+
+}
